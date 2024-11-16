e58acccece0b63d0764b3263f9451ec6e225582b
valentina0612 <vbecag@correo.usbcali.edu.co>
2024-11-16 13:07:33 -0500

    Relación entre entidades

--- src/entities/Favorite.entity.ts
+++ src/entities/Favorite.entity.ts
@@ -1,13 +1,15 @@
-import { Entity, ObjectIdColumn, Column, ObjectId } from "typeorm";
+import { Entity, ObjectIdColumn, Column, ObjectId, ManyToOne } from "typeorm";
+import { User } from "./User.entity";
 
 @Entity({ name: "Favorite" })
 export class Favorite {
     @ObjectIdColumn()
     _id: ObjectId;
 
-    @Column({ nullable: false, unique: true })
-    userId: string;
-
     @Column({ nullable: false })
     characterId: number;
+
+    @ManyToOne(() => User, (user) => user.favorites, { nullable: false })
+    user: User;
+
     }
