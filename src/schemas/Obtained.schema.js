632c52c126871c40af38d671af26288bccff211f
valentina0612 <vbecag@correo.usbcali.edu.co>
2024-11-22 20:32:17 -0500

    Update Obtained schema

--- src/schemas/Obtained.schema.js
+++ src/schemas/Obtained.schema.js
@@ -2,7 +2,10 @@ const Joi = require('joi');
 
 const ObtainedCreationSchema = Joi.object({
     characterId: Joi.number().required(),
-    location : Joi.string().required(),
+    location : Joi.object({
+        lat: Joi.number().required(),
+        lon: Joi.number().required(),
+    }).optional(),
     method : Joi.string().required().valid('Captured', 'Exchanged').insensitive(),
     user: Joi.object({
         _id: Joi.string().required(),
